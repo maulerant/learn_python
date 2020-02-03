a0984f07959bd1184950375ca43174066b41b7c2
Igor Lashyn <ilashyn@gmail.com>
2020-02-03 12:13:25 +0200

    tic-tac-toe with minimax ai

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -22,10 +22,13 @@ c х 0 _
 12. Проверить ситуацию на поле +
 13. если собраны линии по вертикали,горирзонтали или по диагонали написать "проиграл" +
 """
-
+import sys
 from random import randint
 
 VERTICAL_COORDINATS = ('a', 'b', 'c')
+EMPTY_CHAR = '_'
+AI_TURN = True
+USER_TURN = False
 
 
 def get_user_char():
@@ -45,7 +48,7 @@ def show_field(field):
 def is_draw(field):
     count = 0
     for y in range(3):
-        count += 1 if '_' in field[y] else 0
+        count += 1 if EMPTY_CHAR in field[y] else 0
     return count == 0
 
 
@@ -60,7 +63,7 @@ def get_user_position(field):
             continue
 
         real_x, real_y = int(x) - 1, VERTICAL_COORDINATS.index(y)
-        if field[real_y][real_x] == '_':
+        if field[real_y][real_x] == EMPTY_CHAR:
             break
         else:
             print('Position not empty')
@@ -76,39 +79,86 @@ def is_win(char, field):
     opponent_char = get_opponent_char(char)
     # проверяем строки
     for y in range(3):
-        if opponent_char not in field[y] and '_' not in field[y]:
+        if opponent_char not in field[y] and EMPTY_CHAR not in field[y]:
             return True
 
     # проверяем колонки
     for x in range(3):
         col = [field[0][x], field[1][x], field[2][x]]
-        if opponent_char not in col and '_' not in col:
+        if opponent_char not in col and EMPTY_CHAR not in col:
             return True
 
     # проверяем диагонали
     diagonal = [field[0][0], field[1][1], field[2][2]]
-    if opponent_char not in diagonal and '_' not in diagonal:
+    if opponent_char not in diagonal and EMPTY_CHAR not in diagonal:
         return True
     diagonal = [field[0][2], field[1][1], field[2][0]]
-    if opponent_char not in diagonal and '_' not in diagonal:
+    if opponent_char not in diagonal and EMPTY_CHAR not in diagonal:
         return True
 
     return False
 
 
+def minimax(board, depth, is_ai_turn):
+    if is_win(computer_char, board):
+        return scores[computer_char]
+    if is_win(user_char, board):
+        return scores[user_char]
+    if is_draw(board):
+        return scores['draw']
+
+    if is_ai_turn:
+        # выбираем ход который нам выгодней
+        best_score = - sys.maxsize
+        for y in range(3):
+            for x in range(3):
+                if board[y][x] == EMPTY_CHAR:
+                    board[y][x] = computer_char
+                    score = minimax(board, depth + 1, USER_TURN)
+                    board[y][x] = EMPTY_CHAR
+                    best_score = max(best_score, score)
+    else:
+        # противник выбирает ход который нам не выгоден
+        best_score = sys.maxsize
+        for y in range(3):
+            for x in range(3):
+                if board[y][x] == EMPTY_CHAR:
+                    board[y][x] = user_char
+                    score = minimax(board, depth + 1, AI_TURN)
+                    board[y][x] = EMPTY_CHAR
+                    best_score = min(best_score, score)
+    return best_score
+
+
 def get_computer_position(field):
-    x, y = randint(0, 2), randint(0, 2)
-    while field[y][x] != '_':
-        x, y = randint(0, 2), randint(0, 2)
-    return x, y
+    move = None
+    best_score = -sys.maxsize
+    board = [field[y].copy() for y in range(3)]
+    for y in range(3):
+        for x in range(3):
+            if board[y][x] == EMPTY_CHAR:
+                board[y][x] = computer_char
+                score = minimax(board, 0, USER_TURN)
+                board[y][x] = EMPTY_CHAR
+                if score > best_score:
+                    best_score = score
+                    move = (x, y)
+
+    return move
 
 
 field = [
-    ['_' for x in range(3)] for y in range(3)
+    [EMPTY_CHAR for x in range(3)] for y in range(3)
 ]
 
 user_char = get_user_char()
 computer_char = get_opponent_char(user_char)
+
+scores = {
+    user_char: -100,
+    computer_char: 100,
+    'draw': 0
+}
 
 while True:
     show_field(field)
@@ -122,8 +172,12 @@ while True:
         print('you win')
         break
 
-    x, y = get_computer_position(field)
-    field[y][x] = computer_char
-    if is_win(computer_char, field):
-        print('you lose')
-        break
+    move = get_computer_position(field)
+    if move is not None:
+        x, y = move
+        field[y][x] = computer_char
+        if is_win(computer_char, field):
+            print('you lose')
+            break
+
+show_field(field)
